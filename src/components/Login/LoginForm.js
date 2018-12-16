cff7b7df76b8e383146e96a7d4c9e4c858c92fa6
Akshay Milmile <akshay.milmile@gmail.com>
2018-12-16 18:20:25 +0530

    added introduction slides

--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, TouchableOpacity } from 'react-native';
 
 import { RkButton } from 'react-native-ui-kitten';
 import { Entypo } from '@expo/vector-icons';
@@ -8,32 +8,29 @@ import FormInput from '../shared/FormInput';
 import ProductText from '../shared/ProductText';
 
 export default class LoginForm extends React.Component {
-    render() {
-        return (
-            <View style={this.props.style}>
-                <FormInput
-                    icon={<Entypo name="user" />}
-                    placeholder="Email Address"
-                    textContentType="emailAddress"
-                />
-                <FormInput
-                    icon={<Entypo name="lock" />}
-                    placeholder="Password"
-                    textContentType="password"
-                    secureTextEntry={true}
-                />
-                <View>
-                    <ProductText>Forgot Password?</ProductText>
-                </View>
-                <View>
-                    <RkButton
-                        rkType="stretch success"
-                        contentStyle={{ fontFamily: 'product-sans' }}
-                    >
-                        LOGIN
-                    </RkButton>
-                </View>
-            </View>
-        );
-    }
+  render() {
+    return (
+      <View style={this.props.style}>
+        <FormInput
+          icon={<Entypo name="user" />}
+          placeholder="Email Address"
+          textContentType="emailAddress"
+        />
+        <FormInput
+          icon={<Entypo name="lock" />}
+          placeholder="Password"
+          textContentType="password"
+          secureTextEntry={true}
+        />
+        <TouchableOpacity onPress={() => this.props.navigation.navigate('ResetPassword')}>
+          <ProductText>Forgot Password?</ProductText>
+        </TouchableOpacity>
+        <View>
+          <RkButton rkType="stretch success" contentStyle={{ fontFamily: 'product-sans' }}>
+            LOGIN
+          </RkButton>
+        </View>
+      </View>
+    );
+  }
 }
--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -11,48 +11,48 @@ import { white } from '../styles/colors';
 import { space } from '../styles/variables';
 
 export default class LoginScreen extends React.Component {
-    static navigationOptions = {
-        header: null,
-    };
-    render() {
-        return (
-            <View style={styles.container}>
-                <View>
-                    <LogoWithText style={styles.icon} />
-                    <LoginForm style={styles.form} />
-                    <OnboardDivider style={styles.divider} />
-                    <SocialLogin style={styles.social} />
-                    <OnboardNavigation style={styles.onboard} linkToRegister />
-                </View>
-            </View>
-        );
-    }
+  static navigationOptions = {
+    header: null,
+  };
+  render() {
+    return (
+      <View style={styles.container}>
+        <View>
+          <LogoWithText style={styles.icon} />
+          <LoginForm {...this.props} style={styles.form} />
+          <OnboardDivider style={styles.divider} />
+          <SocialLogin style={styles.social} />
+          <OnboardNavigation style={styles.onboard} linkToRegister />
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: white,
-        justifyContent: 'center',
-        paddingLeft: space.base,
-        paddingRight: space.base,
-    },
-    icon: {
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginBottom: space.xl,
-    },
-    form: {
-        marginBottom: space.base,
-    },
-    divider: {
-        marginBottom: space.base,
-    },
-    social: {
-        marginBottom: space.base,
-    },
-    onboard: {
-        alignSelf: 'center',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: white,
+    justifyContent: 'center',
+    paddingLeft: space.base,
+    paddingRight: space.base,
+  },
+  icon: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: space.xl,
+  },
+  form: {
+    marginBottom: space.base,
+  },
+  divider: {
+    marginBottom: space.base,
+  },
+  social: {
+    marginBottom: space.base,
+  },
+  onboard: {
+    alignSelf: 'center',
+  },
 });
