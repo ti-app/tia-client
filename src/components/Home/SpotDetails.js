df7788b77b9f6da59cb0f79b26c736c1f01bafb8
Akshay Milmile <akshay.milmile@gmail.com>
2019-02-17 22:21:46 +0530

    dgasdgasdg

--- src/components/Home/SpotDetails.js
+++ src/components/Home/SpotDetails.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { StyleSheet } from 'react-native';
+import { Container, Content } from 'native-base';
 
 class SpotDetails extends React.Component {
 	constructor(props) {
@@ -8,12 +9,52 @@ class SpotDetails extends React.Component {
 		this.state = {};
 	}
 
+	renderWeekStatus = (weekStatus) => {
+		weekStatus.forEach((aWeek) => <View style={{ ...styles.weekDot, ...styles[aWeek.status] }} />);
+	};
+
 	render() {
-		return <Text>spot details Not impelmnted yet</Text>;
+		return (
+			<Container>
+				<Content>
+					<View style={styles.heading}>
+						<Text style={styles.addressLabel}>Two Stones</Text>
+						<Text style={styles.distanceLabel}>1.3 km FROM HOME</Text>
+					</View>
+					<View style={styles.weekStatus}>
+						{this.renderWeekStatus()}
+						<Text style={styles.lastWateredText}>LAST WATERED ON 05/10/2018 05:55 PM</Text>
+					</View>
+					<Image
+						source={{
+							uri: 'https://media.gettyimages.com/photos/ponthus-beech-picture-id167076876',
+						}}
+						style={{ width: 400, height: 400 }}
+					/>
+					<Text>82 more have watered here</Text>
+				</Content>
+				<Button
+					style={styles.wateredText}
+					success
+					onPress={() => {
+						console.log('watered the tree');
+					}}
+				>
+					<Text> WATERED </Text>
+				</Button>
+			</Container>
+		);
 	}
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	heading: {},
+	addressLabel: {},
+	distanceLabel: {},
+	weekStatus: {},
+	lastWateredText: {},
+	wateredText: { width: '100%' },
+});
 
 const mapDispatchToProps = (dispatch) => ({});
 
