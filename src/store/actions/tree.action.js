7a8e5c75b72cee93db61f5fe00007f589ce76333
Anand Undavia <abundavia@gmail.com>
2019-05-05 21:08:23 +0530

    actions and reducers to incorporate 'plant watering'

--- src/store/actions/tree.action.js
+++ src/store/actions/tree.action.js
@@ -1,3 +1,5 @@
+import { Toast } from 'native-base';
+
 import apiClient from '../../utils/ApiClient';
 import showErrorToast from '../../utils/ErrorToast';
 import NavigationUtil from '../../utils/Navigation';
@@ -5,6 +7,10 @@ import NavigationUtil from '../../utils/Navigation';
 export const ADD_GROUP = 'ADD_GROUP';
 export const FETCH_TREE = 'FETCH_TREE';
 export const FETCH_TREE_SUCCESS = 'FETCH_TREE_SUCCESS';
+export const SET_TREE_SPOT = 'SET_TREE_SPOT';
+export const RESET_TREE_SPOT = 'RESET_TREE_SPOT';
+export const WATER_TREE_SUCCESS = 'WATER_TREE_SUCCESS';
+export const WATER_TREE_FAILURE = 'WATER_TREE_FAILURE';
 
 /**
  * Accepts parameter treeGroup which should be a FormData including an Image.
@@ -50,7 +56,54 @@ export const fetchTrees = (location, radius = 10000, health = 'healthy,weak,almo
 	}
 };
 
+export const waterTree = (tree) => async (dispatch) => {
+	try {
+		const { id } = tree;
+		const url = `/tree/water/${id}`;
+		console.log(`[tree-action::waterTree] making request to "${url}"`);
+		const response = await apiClient({
+			url,
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			noloading: true,
+		});
+		console.log(`[tree-action::waterTree] request to "${url}" was successful`);
+		dispatch(waterTreeSuccess(response.data));
+		Toast.show({
+			text: 'Successfully updated watering details',
+			duration: 1000,
+			textStyle: {
+				textAlign: 'center',
+			},
+		});
+	} catch (err) {
+		showErrorToast('Error fetching nearby trees.', err, dispatch);
+		dispatch(waterTreeFailure(err));
+	}
+};
+
 export const fetchTreeSuccess = (payload) => ({
 	type: FETCH_TREE_SUCCESS,
+	payload,
+});
+
+export const setTreeSpot = (payload) => ({
+	type: SET_TREE_SPOT,
+	payload,
+});
+
+export const resetTreeSpot = () => ({
+	type: RESET_TREE_SPOT,
+});
+
+export const waterTreeSuccess = (payload) => ({
+	type: WATER_TREE_SUCCESS,
+	payload,
+});
+
+// Not used for now
+export const waterTreeFailure = (payload) => ({
+	type: WATER_TREE_FAILURE,
 	payload,
 });
