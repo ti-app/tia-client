c1711b2c34714a0485605f15d7729faec588bdc7
Varit J Patel <5564250+varit05@users.noreply.github.com>
2019-05-16 09:15:22 +0530

    Add reset password functionality (#28)
    * Add reset password functionality
    
    * Fix the issues

--- src/components/ResetPassword/ResetPasswordForm.js
+++ src/components/ResetPassword/ResetPasswordForm.js
@@ -1,17 +1,44 @@
 import React from 'react';
-import { StyleSheet, View, TouchableOpacity } from 'react-native';
-
-// import { RkButton } from 'react-native-ui-kitten';
+import { StyleSheet, View, Text } from 'react-native';
 import { Entypo } from '@expo/vector-icons';
-
 import FormInput from '../shared/FormInput';
-
 import { space } from '../../styles/variables';
 import ProductButton from '../shared/ProductButton';
+import { Toast } from 'native-base';
+import * as firebase from 'firebase';
 
 export default class ResetPasswordForm extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			email: '',
+		};
+	}
+
+	onEmailChange = (email) => {
+		this.setState({ email });
+	};
+
 	onResetClick() {
-		console.log('Reset password clicked');
+		const { email } = this.state;
+		const auth = firebase.auth();
+
+		auth
+			.sendPasswordResetEmail(email)
+			.then(function() {
+				Toast.show({
+					text: `Email has been sent! `,
+					buttonText: 'Okay',
+					type: 'success',
+				});
+			})
+			.catch(function() {
+				Toast.show({
+					text: `Issue while reseting password`,
+					buttonText: 'Okay',
+					type: 'error',
+				});
+			});
 	}
 
 	render() {
@@ -21,10 +48,12 @@ export default class ResetPasswordForm extends React.Component {
 					icon={<Entypo name="user" />}
 					placeholder="Email Address"
 					textContentType="emailAddress"
+					onChangeText={this.onEmailChange.bind(this)}
 				/>
-				<ProductButton full success onPress={this.onResetClick}>
-					SEND PASSWORD RESET LINK
+				<ProductButton full success onPress={this.onResetClick.bind(this)}>
+					<Text>SEND PASSWORD RESET LINK</Text>
 				</ProductButton>
+				<Toast ref="toast" />
 			</View>
 		);
 	}
